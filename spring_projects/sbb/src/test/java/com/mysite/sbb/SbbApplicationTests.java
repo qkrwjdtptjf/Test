4f2b7b1feba031333813a933de9af6cb6740791e
seyeonpark <psy4109@naver.com>
2025-02-05 17:47:10 +0900

    초기화

---
+++ spring_projects/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,105 @@
+package com.mysite.sbb;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+//@SpringBootTest
+//class SbbApplicationTests {
+//
+//	@Autowired
+//	private QuestionRepository questionRepository;
+//
+//	@Autowired
+//	private AnswerRepository answerRepository;
+
+//	@Test //subject값으로 데이터 조회
+//	void testJpa() {
+//		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+//		assertEquals(1, q.getId());
+//	}
+//	@Test //subject값과 content값으로 데이터 조회
+//	void testJpa() {
+//		Question q = this.questionRepository.findBySubjectAndContent(
+//				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+//		assertEquals(1, q.getId());
+//	}
+//	@Test //Like 사용하여 subject 값들 중에 특정 문자열 포함하는 데이터 조회
+//	void testJpa() {
+//		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+//		Question q = qList.get(0);
+//		assertEquals("sbb가 무엇인가요?", q.getSubject());
+//	}
+//	@Test //질문 엔티티의 데이터 수정
+//	void testJpa() {
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		q.setSubject("수정된 제목");
+//		this.questionRepository.save(q);
+//}
+//	@Test //질문 데이터 삭제
+//	void testJpa() {
+//		assertEquals(2, this.questionRepository.count());
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		this.questionRepository.delete(q);
+//		assertEquals(1, this.questionRepository.count());
+//	}
+//	@Test //답변 데이터 저장
+//	void testJpa() {
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//
+//		Answer a = new Answer();
+//		a.setContent("네 자동으로 생성됩니다.");
+//		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+//		a.setCreateDate(LocalDateTime.now());
+//		this.answerRepository.save(a);
+//	}
+//	@Test //답변 데이터 조회
+//	void testJpa() {
+//		Optional<Answer> oa = this.answerRepository.findById(1);
+//		assertTrue(oa.isPresent());
+//		Answer a = oa.get();
+//		assertEquals(2, a.getQuestion().getId());
+//	}
+//	@Transactional ////질문 데이터를 통해 답변 데이터 찾기
+//	@Test
+//	void testJpa() {
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//
+//		List<Answer> answerList = q.getAnswerList();
+//
+//		assertEquals(1, answerList.size());
+//		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+//	}
+//}
+@SpringBootTest     //대량 테스트 데이터 만들기
+class SbbApplicationTests {
+
+	@Autowired
+	private QuestionService questionService;
+
+	@Test
+	void testJpa() {
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content, null);
+		}
+	}
+}
+
+
