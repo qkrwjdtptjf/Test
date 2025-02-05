1ffdd31b367ae37042b3cae9e0e42ffba385ba04
seyeonpark <psy4109@naver.com>
2025-02-05 17:50:39 +0900

    찐초기화

--- spring_projects/sbb/src/main/java/com/mysite/sbb/answer/AnswerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mysite.sbb.answer;
-
-import com.mysite.sbb.answer.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-}
